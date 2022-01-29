ae4372f7d84f1fbef08d6c456d693ada850740a7
mateuszdec <mateusz.dec92@gmail.com>
2022-01-29 11:49:52 +0100

    Dodanie typu wyliczeniowego

---
+++ src/app/Option.java
@@ -0,0 +1,34 @@
+package app;
+
+public enum Option {
+    EXIT(0, "Wyjście z programu"),
+    ADD_BOOK(1, "Dodanie książki"),
+    ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+    PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+    PRINT_MAGAZINES(4, "WYświetlenie dostępnych magazynów/gazet");
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return value + " - " + description;
+    }
+
+    static Option createFromInt(int option) {
+        return Option.values()[option];
+    }
+}
