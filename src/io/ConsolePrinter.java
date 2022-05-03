f197a4607553f19bfeb064190fc8c2178003336a
mateuszdec <mateusz.dec92@gmail.com>
2022-05-03 17:30:30 +0200

    Zmiana w klasie ConsolePrinter

--- src/io/ConsolePrinter.java
+++ src/io/ConsolePrinter.java
@@ -1,43 +1,34 @@
 package io;
 
-import model.Book;
-import model.LibraryUser;
-import model.Magazine;
-import model.Publication;
+import model.*;
 
 import java.util.Collection;
 
 public class ConsolePrinter {
     public void printBooks(Collection<Publication> publications) {
-        int countBooks = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Book) {
-                printLine(publication.toString());
-                countBooks++;
-            }
-        }
-        if (countBooks == 0) {
+        long count = publications.stream()
+                .filter(p -> p instanceof Book)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0)
             printLine("Brak książek w bibliotece");
-        }
     }
 
     public void printMagazines(Collection<Publication> publications) {
-        int countMagazine = 0;
-        for (Publication publication : publications) {
-            if (publication instanceof Magazine) {
-                printLine(publication.toString());
-                countMagazine++;
-            }
-        }
-        if (countMagazine == 0) {
+        long count = publications.stream()
+                .filter(p -> p instanceof Magazine)
+                .map(Publication::toString)
+                .peek(this::printLine)
+                .count();
+        if (count == 0)
             printLine("Brak magazynów w bibliotece");
-        }
     }
 
     public void printUsers(Collection<LibraryUser> users) {
-        for (LibraryUser user : users) {
-            printLine(user.toString());
-        }
+        users.stream()
+                .map(User::toString)
+                .forEach(this::printLine);
     }
 
     public void printLine(String text) {
